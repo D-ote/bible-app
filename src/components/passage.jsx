af647431b79af1cb03e1325c4c8b97c4fc2e7aea
Dooter <dooterior@gmail.com>
2021-07-06 15:49:24 +0100

    feature: passage params

--- src/components/passage.jsx
+++ src/components/passage.jsx
@@ -1,30 +1,30 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const Passage = () => {
   const [chapterPassage, setPassage] = useState([]);
-  const params = useParams();
+  //   const params = useParams();
   const { state } = useLocation();
-  const { details, chapter } = state;
-  const passage = `https://www.abibliadigital.com.br/api/verses/kjv/${details.abbrev.en}/${chapter}`;
 
   useEffect(() => {
     const getPassage = async () => {
       try {
+        const { details, chapter } = state;
+        const passage = `https://www.abibliadigital.com.br/api/verses/kjv/${details.abbrev.en}/${chapter}`;
         const res = await axios.get(passage);
         setPassage(res.data.verses);
       } catch (err) {
-        return err
+        return err;
       }
     };
-      
+
     getPassage();
   }, []);
-  
+
   return (
     <div className="passageDiv">
-    <div className="passageHeader">{details.abbrev.en} chapter {chapter}</div>
+      {/* <div className="passageHeader">{details.abbrev.en} chapter {chapter}</div> */}
       <ul className="passage">
         {chapterPassage.map((chapter) => {
           return <li key={chapter.id}>{chapter.text}</li>;
